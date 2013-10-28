79146b3742e9becc938bbc81a975e6e850fe96da
birbamanuel <birba@et.esiea.fr>
2013-10-28 14:29:27 +0100

    js fonctionnel uniquement en console avec debbugger
    test à effectuer pour le faire s'exécuter dans un js extern au html

--- siteufc/templates/blog/commentaires.js
+++ siteufc/templates/blog/commentaires.js
@@ -1,31 +1,10 @@
-var commentaires = document.getElementById("commentaire");
-commentaires.addEventListener("keyup",comment);
+var message = document.getElementById("commentaire");
+message.addEventListener("keyup",comment);
 
 function comment(){
-							if (commentaires.value.length>6) {
-								alert("time to comment");
+							if (message.value.length>6) {
+								alert(message.value);
 							}
 							
 						}
-window.onload = commentaires;
-
-/*
-var champMotDePass = document.getElementById("champPassword");
-var inputMotDePass = document.getElementById("user_password");
-
-
-
-inputMotDePass.addEventListener("keyup",confirmation);
-
-function confirmation()
-{
-	if (inputMotDePass.value.length<6) {
-		champMotDePass.style.color="bleu";
-	}
-
-	else{
-		champMotDePass.style.color="green";
-		document.body.style.background="red";
-	}
-}
-*/
+window.onload = comment;
