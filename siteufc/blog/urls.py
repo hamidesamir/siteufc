514539ced28b391f6dd6a417113728bff12ea500
root <root@progweb>
2013-10-29 12:23:59 +0100

    reste à mettre à jour la base de donnée, et à modifier certaines tables

--- siteufc/blog/urls.py
+++ siteufc/blog/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('blog.views',
-	url(r'^$', 'accueil'),
-	url(r'^article/(?P<id>\d+)$','lire'),	
+	url(r'^$', 'actu'),
+	url(r'^combattants/(?P<nom_categorie>[a-zA-Z0-9\-_]+)$','article'),	
+	url(r'^combattants/(?P<nom_categorie>[a-zA-Z0-9\-_]+)/(?P<nom_article>[a-zA-Z0-9\-_]+$)','lire'),
 )
--- siteufc/blog/views.py
+++ siteufc/blog/views.py
@@ -7,15 +7,19 @@ from blog.forms import CommentaireForm
 from django.http import Http404
 
 
-def accueil(request):
+def actu(request):
   categorie = Categorie.objects.all()
   return render(request,'blog/actu.html',{'toutes_categories':categorie})
 
 
+def article(request, nom_categorie):
+ categorie = get_object_or_404(Categorie, nom=nom_categorie) 
+ article = Article.objects.filter(categorie__nom__contains=nom_categorie)
+ return render (request, 'blog/article.html',{'categorie':categorie, 'tout_article':article})
 
-def lire(request, id):
- article = get_object_or_404(Article, id=id)
+
+def lire(request, nom_categorie, nom_article):
  
+ article = Article.objects.filter(categorie__nom__contains=nom_categorie).get(titre=nom_article)
  return render (request, 'blog/lire.html', {'article':article})
-
 
