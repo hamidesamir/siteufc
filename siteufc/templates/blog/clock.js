9ea021417f2762d356f6f7254c5943a0f8f83009
hamidesamir <hamidesamir@users.noreply.github.com>
2013-11-01 03:25:32 +0100

    Delete clock.js

--- siteufc/templates/blog/clock.js
+++
@@ -1,8 +0,0 @@
-function displayTime() {
-						var elt = document.getElementById("clock"); // Find element with id="clock"
-						var now = new Date();
-						elt.innerHTML = now.toLocaleTimeString(); 
-						setTimeout(displayTime, 1000);
-}
-
-window.onload = displayTime;
